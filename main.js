d8b2b4034460e9b5fa28c9603ced51c9944510d0
StephLvs <195379@ecam.be>
2026-05-31 22:54:21 +0200

    Show window as an overlay above active workspace

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 // import modules
-const { app, BrowserWindow } = require('electron') 
+const { app, BrowserWindow, screen} = require('electron') 
 
 // initiate window function
 const createWindow = () => {
+  // get current active display 
+  const cursorPoint = screen.getCursorScreenPoint()
+  const currentDisplay = screen.getDisplayNearestPoint(cursorPoint)
+  const {x, y, width, height} = currentDisplay.bounds
+  // create an overlay window
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: width,
+    height: height,
+    x: 0,
+    y: 0,
+
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true,
+    hasShadow: false,
+
+    // remove focus on window
+    focusable: false,
+    show: false,
   })
 
   win.loadFile('index.html')
+  // show overlay across all spaces
+  win.setVisibleOnAllWorkspaces(true, {
+    visibleOnFullScreen: true
+  })
+
+  win.once("ready-to-show", () => {
+    win.showInactive()
+  })
+  // window does not block mouse clicks
+  win.setIgnoreMouseEvents(true)
 }
 
 // when app is ready
