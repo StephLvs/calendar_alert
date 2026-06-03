5ba6deba2f164443005e34f49a5a84eff6b77def
StephLvs <195379@ecam.be>
2026-06-04 01:15:40 +0200

    Reduced  Apple Calendar event lookup  window

--- apple-calendar-test.js
+++ apple-calendar-test.js
@@ -6,33 +6,49 @@ var calendars = calendarApp.calendars();
 // date objects
 var now = new Date();
 var hourRange = new Date(now.getTime() + 60*60*1000);
+var startTimeRange = new Date(now.getTime()+ 4*60*1000);
+var endTimeRange = new Date(now.getTime() + 6*60*1000);
 
-var foundEvents = [];
+var foundEvents = []
 
 
 calendars.forEach(calendar => {
-    // return list of events from the current calendar
-    var events = calendar.events();
+    // return name of current calendar
+    var calendarName = calendar.name();
+    console.log("Checking calendar: " + calendarName);
+
+    // return list of events between time range
+    // >< all events from current calendar
+    var events = calendar.events.whose({
+        _and: [
+            {startDate: {
+                _greaterThan: startTimeRange
+            }},
+            {startDate: {
+                _lessThan: endTimeRange
+            }}
+        ]   
+        })();
+
+    // checks each event from the current calendar
     events.forEach(event => {
         // use calendar properties
         var title = event.summary();
         var startDate = event.startDate();
         var endDate = event.endDate();
 
-        // check if event starts between now and one hour from now
-        if(startDate >= now && startDate <= hourRange) {
-            foundEvents.push({
-                // object (property name: value)
-                title: title,
-                startDate: startDate,
-                endDate: endDate
-            });
-        }
+        foundEvents.push({
+            // object (property name: value)
+            title: title,
+            startDate: startDate,
+            endDate: endDate
+        });
+    
     });  
 });
 
-console.log("Found " + foundEvents.length + " upcoming event(s)");
+console.log("Found " + foundEvents.length + " upcoming event(s) starting in 5 minutes");
 foundEvents.forEach(event => {
-    console.log(event.title + "starts at: " + event.startDate +
+    console.log(event.title + " starts at: " + event.startDate +
         "| ends at: " + event.endDate);
 });
